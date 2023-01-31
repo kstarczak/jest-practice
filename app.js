8664edb8db4f83f1f46884ca8fcc4819b408f2ae
Konrad Starczak <k.starczak@gmail.com>
2023-01-30 21:31:05 -0500

    add 2nd test

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ function capitalize(string) {
     return firstLetter.toUpperCase() + rest;
 }
 
-export { capitalize };
+function reverseString(string) {
+    const strArray = [...string];
+    const length = strArray.length;
+    const reversedArray = [];
+    for (let i = length-1; i >= 0; i--) {
+        reversedArray.push(strArray[i]);
+    };
+    const reversedStr = reversedArray.join('');
+    return reversedStr;
+}
+
+export { capitalize, reverseString };
--- app.test.js
+++ app.test.js
@@ -1,5 +1,9 @@
-import { capitalize } from './app.js';
+import { capitalize, reverseString } from './app.js';
 
 test('makes first letter uppercase', () => {
     expect(capitalize('hello')).toBe('Hello');
+});
+
+test('reverse a string', () => {
+    expect(reverseString("billy")).toBe('yllib');
 });
